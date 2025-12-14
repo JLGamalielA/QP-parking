84015693bcba9368d376ca5a98c304332c638daa
DaniMend17 <lexterlutor12@gmail.com>
2025-12-14 00:31:38 -0600

    routes header updated

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,20 @@
 <?php
 
+/**
+ * Company: CETAM
+ * Project: QPK
+ * File: api.php
+ * Created on: 22/11/2025
+ * Created by: Daniel Yair Mendoza Alvarez
+ * Approved by: Daniel Yair Mendoza Alvarez
+ *
+ * Changelog:
+ * - ID: 1 | Modified on: 22/11/2025 |
+ *   Modified by: Daniel Yair Mendoza Alvarez |
+ *   Description: Route definitions for api |
+ * 
+ */
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
--- routes/web.php
+++ routes/web.php
@@ -10,8 +10,9 @@
  *
  * Changelog:
  * - ID: 1 | Modified on: 22/11/2025 |
- * Modified by: Daniel Yair Mendoza Alvarez |
- * Description: Route definitions for QParking application following CETAM standards. |
+ *   Modified by: Daniel Yair Mendoza Alvarez |
+ *   Description: Route definitions for QParking application following CETAM standards |
+ * 
  */
 
 use App\Http\Controllers\Admin\AdminDashboardController;
@@ -53,12 +54,6 @@ use App\Livewire\ResetPasswordExample;
 use App\Livewire\UpgradeToPro;
 use App\Livewire\Users;
 
-/*
-  Web Routes
-  Here is where you can register web routes for your application. These
-  routes are loaded by the RouteServiceProvider within a group which
-   contains the "web" middleware group. Now create something great!
- */
 
 // Configurable project prefix: /p/<slug>
 $slug = config('proj.slug');
@@ -112,7 +107,6 @@ Route::prefix("p/{$slug}")
                 Route::resource('parking-entries', ParkingEntryController::class);
 
                 Route::resource('parking-entries.manual-access', ManualAccessController::class);
-
                 // Active QR Scans
                 Route::resource('active-user-qr-scans', ActiveUserQrScanController::class);
 
